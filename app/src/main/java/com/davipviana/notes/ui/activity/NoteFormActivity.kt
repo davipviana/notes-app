3005c4ed5ec0da6efeff2d12233e3b3a460a3bb0
Davi Viana <davipviana@gmail.com>
2018-11-15 16:10:28 -0200

    Note form menu

--- app/src/main/java/com/davipviana/notes/ui/activity/NoteFormActivity.kt
+++ app/src/main/java/com/davipviana/notes/ui/activity/NoteFormActivity.kt
@@ -2,6 +2,8 @@ package com.davipviana.notes.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import com.davipviana.notes.R
 
 class NoteFormActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class NoteFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_form)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.note_form_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
