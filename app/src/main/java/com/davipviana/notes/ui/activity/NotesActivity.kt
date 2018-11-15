03c82f33006929fe1b1d4ed1fd0e3b92d089b65e
Davi Viana <davipviana@gmail.com>
2018-11-15 15:43:58 -0200

    New note form layout

--- app/src/main/java/com/davipviana/notes/ui/activity/NotesActivity.kt
+++ app/src/main/java/com/davipviana/notes/ui/activity/NotesActivity.kt
@@ -1,9 +1,10 @@
 package com.davipviana.notes.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.TextView
 import com.davipviana.notes.R
 import com.davipviana.notes.dao.NoteDao
 import com.davipviana.notes.model.Note
@@ -16,6 +17,15 @@ class NotesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_notes)
 
         initializeNotesRecyclerView(getExampleNotes())
+
+        initializeNewNoteClick()
+    }
+
+    private fun initializeNewNoteClick() {
+        val newNoteTextView = findViewById<TextView>(R.id.notes_new_note)
+        newNoteTextView.setOnClickListener {
+            startActivity(Intent(this, NoteFormActivity::class.java))
+        }
     }
 
     private fun getExampleNotes(): List<Note> {
